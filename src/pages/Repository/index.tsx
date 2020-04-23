7c9d2f42fbe5a863b3baed2dd1c825d70233a1fe
Rodrigo Garcia <rgflsc@gmail.com>
2020-04-23 03:22:50 -0300

    Estilizando Detalhe
    Estilizando Detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImage from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,51 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImage} alt="Github Explorer" srcSet="" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>sdfds</strong>
+            <p>sdfss</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>fdsfsdf</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>fdsfsdf</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>fdsfsdf</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="dfdfg">
+          <div>
+            <strong>sdfsd</strong>
+            <p>sfgsd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
