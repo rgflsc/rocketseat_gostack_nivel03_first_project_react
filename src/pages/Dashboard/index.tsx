edc7e269e2f04c4f9534eeca2930af1fc4e9dcc0
Rodrigo Garcia <rgflsc@gmail.com>
2020-04-23 01:22:35 -0300

    Estilizando Dashboard
    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImage from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios do Github</Title>;
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" srcSet="" />
+      <Title>Explore repositórios do Github</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/17195743?s=460&u=b5b229154b57910950c1e5da4955bd8073cecffc&v=4"
+            alt="Rodrigo Garcia"
+          />
+          <div>
+            <strong>dfgfdgdfgfdgdfgfdgfdgdfgfdgdfgdf</strong>
+            <p>345345435346534634663634634</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/17195743?s=460&u=b5b229154b57910950c1e5da4955bd8073cecffc&v=4"
+            alt="Rodrigo Garcia"
+          />
+          <div>
+            <strong>dfgfdgdfgfdgdfgfdgfdgdfgfdgdfgdf</strong>
+            <p>345345435346534634663634634</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/17195743?s=460&u=b5b229154b57910950c1e5da4955bd8073cecffc&v=4"
+            alt="Rodrigo Garcia"
+          />
+          <div>
+            <strong>dfgfdgdfgfdgdfgfdgfdgdfgfdgdfgdf</strong>
+            <p>345345435346534634663634634</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
